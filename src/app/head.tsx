be751b966530a127387b2e0c69e46192f998df8f
Audart Lucas <lucas.audart@ocea-sb.com>
2023-03-05 00:54:49 +0100

    🥇 add first version of ranking page

---
+++ src/app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=VT323&display=swap"
+        rel="stylesheet"
+      />
+    </head>
+  );
+}
